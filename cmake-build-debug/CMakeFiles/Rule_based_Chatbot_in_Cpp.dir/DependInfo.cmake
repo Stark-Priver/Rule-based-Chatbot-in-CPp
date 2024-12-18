99eab695973bed1b2da5128efcc2ec7f77fa7009
Priver <priverdeprince@yahoo.com>
2024-12-18 11:59:41 +0300

    Modified the Project

---
+++ cmake-build-debug/CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/priver/Studies/Rule based Chatbot in Cpp/src/chatbot.cpp" "CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/src/chatbot.cpp.o" "gcc" "CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/src/chatbot.cpp.o.d"
+  "/home/priver/Studies/Rule based Chatbot in Cpp/src/main.cpp" "CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/src/main.cpp.o" "gcc" "CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/src/main.cpp.o.d"
+  "/home/priver/Studies/Rule based Chatbot in Cpp/src/system_controls.cpp" "CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/src/system_controls.cpp.o" "gcc" "CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/src/system_controls.cpp.o.d"
+  "/home/priver/Studies/Rule based Chatbot in Cpp/src/utilities.cpp" "CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/src/utilities.cpp.o" "gcc" "CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/src/utilities.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/src/chatbot.cpp.o"
+  "CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/src/chatbot.cpp.o.d"
+  "CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/src/main.cpp.o"
+  "CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/src/main.cpp.o.d"
+  "CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/src/system_controls.cpp.o"
+  "CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/src/system_controls.cpp.o.d"
+  "CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/src/utilities.cpp.o"
+  "CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/src/utilities.cpp.o.d"
+  "Rule_based_Chatbot_in_Cpp"
+  "Rule_based_Chatbot_in_Cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rule_based_Chatbot_in_Cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
