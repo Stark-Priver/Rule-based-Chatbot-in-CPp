b7f9b7d754a11f7764d39bd56ad7494df224169f
Priver <priverdeprince@yahoo.com>
2024-12-19 20:31:56 +0300

    Added the file to handle speech recogniton logic

---
+++ include/vosk.h
@@ -0,0 +1,8 @@
+//
+// Created by priver on 12/19/24.
+//
+
+#ifndef VOSK_H
+#define VOSK_H
+
+#endif //VOSK_H
--- src/speech_recognition.cpp
+++ src/speech_recognition.cpp
@@ -8,36 +8,7 @@
 #include <fstream>
 #include <memory>
 #include <vosk.h>
-
+// TODO --> Implementing the logic to handle the speech recognition
 void listenToVoiceInput(std::string& recognizedText) {
-    // Initializing  Vosk model
-    std::string model_path = "//";
-    vosk::Model model(model_path);
-
-    // Create recognizer for audio stream (16000 Hz sample rate)
-    std::shared_ptr<vosk::Recognizer> recognizer = std::make_shared<vosk::Recognizer>(model, 16000.0);
-
-    // Open the microphone input  for linux based os
-    std::ifstream audio_stream("/dev/audio", std::ios::binary);
-    if (!audio_stream) {
-        std::cerr << "Error opening audio stream\n";
-        return;
-    }
-
-    char buffer[4000];
-    std::string result_text;
-
-    // Processing the audio input and converting it to text
-    while (audio_stream.read(buffer, sizeof(buffer))) {
-        if (recognizer->AcceptWaveform(buffer, sizeof(buffer))) {
-            std::string result = recognizer->Result();
-            std::cout << "Recognized text: " << result << std::endl;
-            result_text = result;
-        }
-    }
-
-    // Final result after speech input ends
-    std::string final_result = recognizer->FinalResult();
-    std::cout << "Final recognized text: " << final_result << std::endl;
-    recognizedText = final_result;  // Returning the recognized text to the caller
+    // The logic to handle the speech recognition
 }
